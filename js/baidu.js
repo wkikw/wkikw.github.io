03586e5e178c4726c42a21d4832412d4636d65e1
xyz <xieyinzhangl@gmail.com>
2022-10-28 09:29:20 +0800

    Site updated: 2022-10-28 09:29:15

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+ var _hmt = _hmt || [];
+ (function() {
+     var hm = document.createElement("script");
+     hm.src = "https://hm.baidu.com/hm.js?4629f826b7c1dd5909efd8529201e433";
+     var s = document.getElementsByTagName("script")[0];
+     s.parentNode.insertBefore(hm, s);
+ })();
